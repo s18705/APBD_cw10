ac4198cffec966ce7450ac2f05ab5143f48b716a
Karolina <s18705@pjwstk.edu.pl>
2021-01-28 22:46:27 +0100

    cw 10 final version

---
+++ cw10/cw10/Controllers/EnrollmentsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using cw10.Models;
+using cw10.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw10.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        private readonly s18705Context db;
+
+        public EnrollmentsController(s18705Context _db)
+        {
+            db = _db;
+        }
+
+        [HttpPost]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        {
+            if (!(db.Student.Where(s => s.IndexNumber == request.IndexNumber).Any()))
+            {
+                if (db.Studies.Where(s => s.Name == request.Studies).Any())
+                {
+                    var studies = db.Studies.SingleOrDefault(s => s.Name == request.Studies);
+                    var date = db.Enrollment.Max(d => d.StartDate);
+
+                    if (!(db.Enrollment.Where(e => (e.Semester == 1) && (e.IdStudy == studies.IdStudy) && (e.StartDate == date)).Any()))
+                    {
+                        int id = db.Enrollment.Max(i => i.IdEnrollment);
+                        Enrollment enrollment = new Enrollment
+                        {
+                            IdEnrollment = id,
+                            Semester = 1,
+                            IdStudy = studies.IdStudy,
+                            StartDate = Convert.ToDateTime(DateTime.Now)
+                        };
+                        db.Enrollment.Add(enrollment);
+                        db.SaveChanges();
+                    }
+
+                    var enroll = db.Enrollment.SingleOrDefault(e => (e.Semester == 1) && (e.IdStudy == studies.IdStudy) && (e.StartDate == date));
+                    Student student = new Student
+                    {
+                        IndexNumber = request.IndexNumber,
+                        FirstName = request.FirstName,
+                        LastName = request.LastName,
+                        BirthDate = Convert.ToDateTime(request.Birthdate),
+                        IdEnrollment = enroll.IdEnrollment
+                    };
+                    db.Student.Add(student);
+                    db.SaveChanges();
+                    return Ok("Dodano nowego studenta");
+                }
+            }
+            return NotFound("Podano błędnie, któryś z argumentów wew. requesta");
+        }
+    }
+}
---
+++ cw10/cw10/Controllers/StudentsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using cw10.Models;
+using cw10.Requests;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace cw10
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly s18705Context db;
+        public StudentsController()
+        {
+            db = new s18705Context();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(String id)
+        {
+            if (db.Student.Where(s => s.IndexNumber == id).Any())
+            {
+                var res = db.Student.SingleOrDefault(s => s.IndexNumber == id);
+                db.Student.Remove(res);
+                String result = res.ToString();
+                db.SaveChanges();
+                return Ok("Usunieto " + result);
+            }
+            return NotFound("Nie ma ucznia o takim id");
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok(db.Student.ToList());
+        }
+
+
+
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(String id)
+        {
+            if (db.Student.Where(s => s.IndexNumber == id).Any())
+            {
+                var res = db.Student.SingleOrDefault(s => s.IndexNumber == id);
+                res.Modified = DateTime.Now;
+                db.SaveChanges();
+                return Ok("Zmodyfikowano "+DateTime.Now);
+            }
+            return NotFound("Podano zly index");
+        }
+      
+
+        [Route("/api/students/promotions")]
+        [HttpPost]
+        public IActionResult PromoteStudents(PromoteStudent promotions)
+        {
+            var res = from enroll in db.Enrollment join studies in db.Studies on enroll.IdStudy equals studies.IdStudy where enroll.Semester == promotions.Semester && studies.Name == promotions.Studies select enroll;
+
+            if (res!=null)
+            {
+                var data = new object[2] { promotions.Studies, promotions.Semester };
+                db.Database.ExecuteSqlRaw("EXEC PromoteStudents @StudiesName,@Semester", data);
+                db.SaveChanges();
+                return Ok("Student awansowal na kolejny semestr");
+            }
+
+            return NotFound();
+        }
+    }
+}
